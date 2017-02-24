a55414e84624401033619cfc8f80fb906e379514
zesteros <angelo_384@hotmail.com>
2017-02-23 23:32:27 -0600

    [ADD] First Commit

---
+++ src/main/java/itl/skinart/MainActivity.java
@@ -0,0 +1,28 @@
+package itl.skinart;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by Angelo on 04/02/2017.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, UnityPlayerActivity.class));
+            }
+        });
+    }
+}
